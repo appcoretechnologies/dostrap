8783b17f4b4f068b1a1e1d1c85df7551700f8b1f
amit.poojary.15@gmail.com <amit.poojary.15@gmail.com>
2015-02-09 22:07:38 +0530

    Completed Custom Button Widget.

--- src/main/resources/webroot/weblabs/buttons/button.js
+++ src/main/resources/webroot/weblabs/buttons/button.js
@@ -1,54 +1,100 @@
 define(['dojo/_base/declare',
     'dijit/_Widget',
-    'dijit/_Templated',
+    'dijit/_TemplatedMixin',
     "dojo/text!./templates/Button.html",
-    //'dojo/html',
-   // 'dojo/dom',
-   // 'dojo/on',
-    'dojo/domReady!'
-],function(declare,_Widget,_Templated,templateString, dom){
-    //on(dom.byId("setContent").value);
-  // this.set("label", content);
-
-   // dojo.ready(function() {
-        //...make elements into widgets
-    //    var widget = new dojo.byId("makeMeAWidget"); //options,elementID
-   // });
-   // this.set("label", widget);
+    'dojo/domReady!',
+    'dojo/dom-class'
+], function (declare, _Widget, _Templated, templateString, dom, domClass) {
 
 
     return declare([ _Widget, _Templated], {
         widgetsInTemplate: true,
         templateString: templateString,
-       // showLabel: false,
-        label:"default",
-        setLabel: function(/*DomNode*/ label){
-            this._set("label", label);
-            console.debug("TESt"+label);
-        }
+        showLabel: false,
+        size: "medium",
+        label: "default",
+        buttonClass: " btn btn-default ",
+        type: "default",
+        setButtonClass: function () {
+            var buttonClass = "btn";
+            switch (this.size) {
+                case "small":
+                    buttonClass += " btn-sm";
+                    break;
+                case "medium":
+                    buttonClass += " btn-lg";
+                    break;
+                case "xsmall":
+                    buttonClass += " btn-xs";
+                    break;
+            }
+            switch (this.type) {
+                case "default":
+                    buttonClass += " btn-default";
+                    break;
+                case "primary":
+                    buttonClass += " btn-primary";
+                    break;
+                case "success":
+                    buttonClass += " btn-success";
+                    break;
+                case "info":
+                    buttonClass += " btn-info";
+                    break;
+                case "warning":
+                    buttonClass += " btn-warning";
+                    break;
+                case "danger":
+                    buttonClass += " btn-danger";
+                    break;
 
-       // _fillContent: function(/*DomNode*/ source){
+            }
+            this.buttonClass = buttonClass;
+            this.titleNode.className = this.buttonClass;
+        },
+        _setLabelAttr: function (/*String*/ content) {
+            // summary:
+            //		Hook for set('label', ...) to work.
+            // description:
+            //		Set the label (text) of the button; takes an HTML string.
+            //		If the label is hidden (showLabel=false) then and no title has
+            //		been specified, then label is also set as title attribute of icon.
+            this.inherited(arguments);
+            this.titleNode.innerHTML = content;
+
+        },
+
+        _setSizeAttr: function (/*String*/ size) {
+            this.inherited(arguments);
+            this.size = size;
+            this.setButtonClass();
+        },
+
+        postMixInProperties: function () {
+            this.inherited(arguments);
+
+        },
+
+        _setTypeAttr: function (/*String*/ type) {
+            this.inherited(arguments);
+            this.type = type;
+            this.setButtonClass();
+        },
+
+        _fillContent: function (/*DomNode*/ source) {
             // Overrides _Templated._fillContent().
             // If button label is specified as srcNodeRef.innerHTML rather than
             // this.params.label, handle it here.
             // TODO: remove the method in 2.0, parser will do it all for me
-       //     if(source && (!this.params || !("label" in this.params))){
-          //      var sourceLabel = lang.trim(source.innerHTML);
-            //    if(sourceLabel){
-           //         this.label = sourceLabel; // _applyAttributes will be called after buildRendering completes to update the DOM
-           //   }
-          //  }
-      //  }
+            console.debug("size " + this.size);
+            console.debug("type" + this.type);
 
-        //   setLabel: function(/*String*/ content){
-        // summary:
-            //		Deprecated.  Use set('label', ...) instead.
-      //      kernel.deprecated("dijit.form.Button.setLabel() is deprecated.  Use set('label', ...) instead.", "", "2.0");
-       //     this.set("label", "Amit");
-     //   }
-        //buildRendering: function(){
-            // create the DOM for this widget
-          //  this.domNode = domConstruct.create("button", {innerHTML: "push me"});
-      //  }
+            if (source && (!this.params || !("label" in this.params))) {
+                var sourceLabel = source.innerHTML;
+                if (sourceLabel) {
+                    this.label = sourceLabel; // _applyAttributes will be called after buildRendering completes to update the DOM
+                }
+            }
+        }
     });
 });
